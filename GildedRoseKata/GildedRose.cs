83ad0fb1fcd14d87130cc283458a42044293e445
BBPC\yeiso <yeisonpx@gmail.com>
2023-05-17 22:47:20 -0400

    Reduce conditional complexity.

--- GildedRoseKata/GildedRose.cs
+++ GildedRoseKata/GildedRose.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using Xunit;
 
@@ -7,6 +8,13 @@ namespace GildedRoseKata
 {
     public class GildedRose
     {
+        private readonly string[] _specialProducts = new string[]
+        {
+            ItemNames.AgedBrie,
+            ItemNames.BackstagePasses,
+            ItemNames.SulfurasHandOfRagnaros
+        };
+
         IList<Item> Items;
         public GildedRose(IList<Item> Items)
         {
@@ -17,56 +25,37 @@ namespace GildedRoseKata
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && 
-                    Items[i].Name != "Backstage passes to a TAFKAL80ETC concert" && 
-                    Items[i].Name != "Sulfuras, Hand of Ragnaros" &&
-                    Items[i].Quality > 0)
-                {
-                    Items[i].Quality = Items[i].Quality - 1;
-                }
-                else if (Items[i].Quality < 50)
-                {
-                    Items[i].Quality = Items[i].Quality + 1;
-                }
+                var isSpecialProduct = _specialProducts.Contains(Items[i].Name);
+                var isNormalProduct = !isSpecialProduct && Items[i].Quality > 0;
+                if (isNormalProduct)
+                    Items[i].Quality--;
 
-                if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert" &&
-                    Items[i].Quality < 50 &&
-                    Items[i].SellIn < 11)
-                {
-                    Items[i].Quality = Items[i].Quality + 1;
-                }
+                var isSpecialProductWithGoodQuality = isSpecialProduct && Items[i].Quality < 50;
+                if (isSpecialProductWithGoodQuality)
+                    Items[i].Quality++;
 
-                if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert" &&
-                    Items[i].Quality < 50 &&
-                    Items[i].SellIn < 6)
-                {
-                    Items[i].Quality = Items[i].Quality + 1;
-                }
-
+                var isValidBackstagePasses = Items[i].Name == "Backstage passes to a TAFKAL80ETC concert" && Items[i].Quality < 50;
+                if (isValidBackstagePasses && Items[i].SellIn < 11)
+                    Items[i].Quality++;
+                
+                if (isValidBackstagePasses && Items[i].SellIn < 6)
+                    Items[i].Quality++;
+                
                 if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                
-                if(Items[i].SellIn < 0 && 
-                   Items[i].Name == "Aged Brie" && 
-                   Items[i].Quality < 50)
-                    Items[i].Quality = Items[i].Quality + 1;
-            
-                if (Items[i].SellIn < 0 && 
-                    Items[i].Name != "Aged Brie" && 
-                    Items[i].Name != "Backstage passes to a TAFKAL80ETC concert" && 
-                    Items[i].Name != "Sulfuras, Hand of Ragnaros" &&
-                    Items[i].Quality > 0)
-                {
-                    Items[i].Quality = Items[i].Quality - 1;
-                }
-                
-                if(Items[i].SellIn < 0 && 
-                   Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                {
+                    Items[i].SellIn--;
+
+                var isProductSellByDatePassed = Items[i].SellIn < 0;
+                if (isProductSellByDatePassed && Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                     Items[i].Quality = 0;
-                }
-                
+
+                if (isProductSellByDatePassed && Items[i].Name == "Aged Brie" &&  Items[i].Quality < 50)
+                    Items[i].Quality++;
+            
+                if (isProductSellByDatePassed && !isSpecialProduct && Items[i].Quality > 0)
+                    Items[i].Quality--;
             }
         }
+
+
     }
 }
